0cb18ef7129f496823c935a02c9c47c72e301188
lsv6772 <117819102+lsv6772@users.noreply.github.com>
2023-04-26 23:34:56 +0900

    Add files via upload

---
+++ Programers Arhorithm/day1/quotient.js
@@ -0,0 +1,6 @@
+function solution(num1, num2) {
+  if(0 < num1 <= 100 && 0 < num2 <= 100){
+      var answer = parseInt(num1 / num2);
+      return answer;    
+  }   
+}
---
+++ Programers Arhorithm/day2/두 수의 나눗셈.js
@@ -0,0 +1,14 @@
+function solution(num1, num2) {
+  var answer = parseInt((num1 / num2) * 1000);
+  return answer;
+}
+
+// const solution = (num1, num2) => Math.floor(num1 / num2 * 1000)
+
+// function solution(num1, num2) {
+//     return Math.trunc(num1 / num2 * 1000);
+// }
+
+// function solution(num1, num2) {
+//     return ~~(num1/num2*1000);
+// }
